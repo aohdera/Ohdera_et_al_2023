5b2c8b06c2b5ed235a331f3611a5aadae46c963c
Aki <aohdera29@gmail.com>
2023-03-22 14:53:18 -0700

    Update qiime2.sh

--- 02-16S-Amplicon_Analysis/qiime2.sh
+++ 02-16S-Amplicon_Analysis/qiime2.sh
@@ -5,7 +5,7 @@
 qiime tools import --type 'SampleData[PairedEndSequencesWithQuality]' --input-path manifest.csv --output-path paired-end-demux.qza --input-format PairedEndFastqManifestPhred33V2
 
 #dada2: trim, filter, merge
-qiime dada2 denoise-paired --i-demultiplexed-seqs paired-end-demux.qza --p-trunc-len-f 178 --p-trunc-len-r 180  --p-trim-left-f 0 --p-trim-left-r 0 --p-trunc-q 2 --o-table dada2-table.qza --o-representative-sequences dada2-rep-seqs.qza --o-denoising-stats denoise.statsqiime feature-table tabulate-seqs --i-data rep-seqs.qza --o-visualization rep-seqs.qzv
+qiime dada2 denoise-paired --i-demultiplexed-seqs paired-end-demux.qza --p-trunc-len-f 250 --p-trunc-len-r 180  --p-trim-left-f 0 --p-trim-left-r 0 --p-trunc-q 2 --o-table dada2-table.qza --o-representative-sequences dada2-rep-seqs.qza --o-denoising-stats denoise.statsqiime feature-table tabulate-seqs --i-data rep-seqs.qza --o-visualization rep-seqs.qzv
 qiime feature-table filter-features --i-table dada2.table.qza --p-min-frequency 5 --p-min-samples 2 --o-filtered-table dada2.table.filtered.qza
 qiime feature-table summarize --i-table dada2.table.filtered.qza --o-visualization dada2.table.filtered.qzv --m-sample-metadata-file metadata.txt
 
